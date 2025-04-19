8a4a4b617f74b7768e14e1cf1703abcb7f4e5971
Carbone404111 <leandrebc111@outlook.fr>
2025-04-19 10:38:45 +0200

    app.py

--- app.py
+++ app.py
@@ -20,8 +20,12 @@ def index():
         try:
             h_km = float(request.form['distance'])
 
-            if h_km >= 326000:
-                result1 = "⚠️ Ce satellite n'en est pas un : il est au point de Lagrange L1."
+            if h_km >= 326000 or h_km == 326000:
+                result1 = "⚠️ Ce satellite est trop loin de la terre, il a dépassé le point L1, et l'influence de la terre est négligeable."
+                result2 = "Fais une recherche pour en savoir plus !"
+                return render_template('index.html', result1=result1, result2=result2)
+            if h_km <= 160 or h_km == 160:
+                result1 = "⚠️ Ce satellite est trop proche de la terre, il peut exister brièvement, mais ce n’est pas une orbite stable.."
                 result2 = "Fais une recherche pour en savoir plus !"
                 return render_template('index.html', result1=result1, result2=result2)
 
